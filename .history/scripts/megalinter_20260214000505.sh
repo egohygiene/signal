4dca321f53a845ad4c747c12bcc5074c0fcbb935
Alan Szmyt <szmyty@gmail.com>
2026-02-14 00:14:47 -0500

    Update

---
+++ .history/scripts/megalinter_20260214000505.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker run \
+  --rm \
+  --interactive \
+  --tty \
+  --platform "linux/amd64" \
+  --volume "$(pwd)":/github/workspace \
+  --workdir /github/workspace \
+  --env GITHUB_WORKSPACE="/github/workspace" \
+  --env GITHUB_REPOSITORY="egohygiene/signal" \
+  --env GITHUB_REF="refs/heads/main" \
+  --env GITHUB_RUN_ID="local-run" \
+  --env PYTHONWARNINGS="error" \
+  oxsecurity/megalinter:v8.8.0
+
